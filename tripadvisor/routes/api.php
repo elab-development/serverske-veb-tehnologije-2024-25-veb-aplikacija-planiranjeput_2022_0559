70f0467fbf07707a1ae94eb34297ce043582d2d3
teaaaa3333 <janicijevic.t03@gmail.com>
2025-10-01 08:48:59 +0200

    destination-controllers-and-routes

--- tripadvisor/routes/api.php
+++ tripadvisor/routes/api.php
@@ -30,3 +30,7 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     });
+    
+
+    Route::resource('destinations', DestinationController::class)
+        ->only(['store', 'update', 'destroy']);
