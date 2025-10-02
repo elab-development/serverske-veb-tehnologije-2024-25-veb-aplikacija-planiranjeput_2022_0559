e6f7bc1d52dd359ff821859fed6ec8c8caa6ddb3
teaaaa3333 <janicijevic.t03@gmail.com>
2025-10-02 23:40:05 +0200

    review-controllers-and-routes

--- tripadvisor/app/Http/Controllers/ReviewController.php
+++ tripadvisor/app/Http/Controllers/ReviewController.php
@@ -79,9 +79,7 @@ class ReviewController extends Controller
         ], 201);
     }
 
-    /**
-     * Display the specified resource.
-     */
+    
     public function show(Review $review)
     {
         //
